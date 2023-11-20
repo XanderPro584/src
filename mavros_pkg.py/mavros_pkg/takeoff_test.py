e7ded0c165ce72b92e2b1160237118c0293e8265
Alexander Agrawal <agrawal.alexander@gmail.com>
2023-11-20 13:17:44 -0600

    before using my_robot_description

---
+++ mavros_pkg.py/mavros_pkg/takeoff_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from mavros_msgs.msg import PositionTarget
+
+
+class TakeoffTestNode(Node): # MODIFY NAME
+    def __init__(self):
+        super().__init__("takeoff_test") # MODIFY NAME
+        self.position_publisher = self.create_publisher(PositionTarget, '/mavros/setpoint_position/local', 10)
+        self.takeoff_command = PositionTarget()
+        self.takeoff_command.coordinate_frame = PositionTarget.FRAME_LOCAL_NED
+        self.takeoff_command.type_mask = PositionTarget.IGNORE_PX + PositionTarget.IGNORE_PY + PositionTarget.IGNORE_PZ + PositionTarget.IGNORE_AFX + PositionTarget.IGNORE_AFY + PositionTarget.IGNORE_AFZ
+        self.takeoff_command.position.z = 10.0
+
+        self.timer_ = self.create_timer(0.1, self.publish_position)
+
+    def publish_position(self):
+        self.position_publisher.publish(self.takeoff_command)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TakeoffTestNode() # MODIFY NAME
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
