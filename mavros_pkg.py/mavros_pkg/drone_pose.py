b1bee78d320fc516799e9cc9398e8d7139b7024e
Alexander Agrawal <agrawal.alexander@gmail.com>
2023-12-03 18:44:48 -0600

    Set up local positioning

--- mavros_pkg.py/mavros_pkg/drone_pose.py
+++ mavros_pkg.py/mavros_pkg/drone_pose.py
@@ -27,7 +27,7 @@ class DroneControllerNode(Node): # MODIFY NAME
             history=HistoryPolicy.KEEP_LAST,
             depth=1
         )
-
+        # Initialization
         self.current_state_g = State()
         self.current_pose_g = Odometry()
         self.correction_vector_g = Pose()
@@ -39,22 +39,28 @@ class DroneControllerNode(Node): # MODIFY NAME
         self.correction_heading_g = 0.0
         self.local_desired_heading_g = 0.0
 
+        self.frame_initialized = False
+
+        # set up publishers and subscribers
         self.local_position_pub = self.create_publisher(Point, "/local_position", qos_profile=qos_profile)
         self.current_heading_pub = self.create_publisher(Float64, "/local_heading", qos_profile=qos_profile)
         self.currentPos = self.create_subscription(Odometry, "/mavros/global_position/local", self.pose_cb, qos_profile=qos_profile)
         self.state_sub = self.create_subscription(State, "/mavros/state", self.state_cb, qos_profile=qos_profile)
         self.local_pos_pub = self.create_publisher(PoseStamped, "/mavros/setpoint_position/local", qos_profile=qos_profile)
 
-        self.arming_client = self.create_client(CommandBool, "/mavros/cmd/arming")
-        self.land_client = self.create_client(CommandTOL, "/mavros/cmd/land")
-        self.takeoff_client = self.create_client(CommandTOL, "/mavros/cmd/takeoff")
-        self.set_mode_client = self.create_client(SetMode, "/mavros/set_mode")
-        self.command_client = self.create_client(CommandLong, "/mavros/cmd/command")
+        # self.arming_client = self.create_client(CommandBool, "/mavros/cmd/arming")
+        # self.land_client = self.create_client(CommandTOL, "/mavros/cmd/land") 
+        # self.takeoff_client = self.create_client(CommandTOL, "/mavros/cmd/takeoff")
+        # self.set_mode_client = self.create_client(SetMode, "/mavros/set_mode")
+        # self.command_client = self.create_client(CommandLong, "/mavros/cmd/command")
 
         self.get_logger().info("Drone Controller Node has been initialized")
+        
+        # Connect then initialize local frame
+        if self.wait4connect() != -1:
+            self.initialize_local_frame()
 
-        self.initialize_local_frame()
-
+        
     def state_cb(self,msg):
         self.current_state_g = msg
 
@@ -68,13 +74,16 @@ class DroneControllerNode(Node): # MODIFY NAME
             self.current_pose_g.pose.pose.orientation.y,
             self.current_pose_g.pose.pose.orientation.z,
         )
+        # self.get_logger().info(f"current_pose_g: {self.current_pose_g.pose.pose.orientation}")
+        # self.get_logger().info(f"local_offset_g: {self.local_offset_g}")
 
         psi = atan2((2 * (q0 * q3 + q1 * q2)),
                     (1 - 2 * (pow(q2, 2) + pow(q3, 2))))
         
         self.current_heading_g = degrees(psi) - self.local_offset_g
         # self.get_logger().info("current_heading_g: {}".format(self.current_heading_g))
-        self.current_heading_pub.publish(Float64(data=self.current_heading_g))
+        if self.frame_initialized:
+            self.current_heading_pub.publish(Float64(data=self.current_heading_g))
 
     def enu_2_local(self):
         x, y, z = (
@@ -95,13 +104,14 @@ class DroneControllerNode(Node): # MODIFY NAME
 
         current_pos_local.z = z
         # self.get_logger().info("current_pos_local: {}".format(current_pos_local))
-        self.local_position_pub.publish(current_pos_local)
+        if self.frame_initialized:
+            self.local_position_pub.publish(current_pos_local)
         return current_pos_local
 
     def initialize_local_frame(self):
         self.local_offset_g = 0.0
 
-        for i in range(30):
+        for i in range(60):
             rclpy.spin_once(self, timeout_sec=0.1)
             time.sleep(0.1)
 
@@ -120,75 +130,28 @@ class DroneControllerNode(Node): # MODIFY NAME
             self.local_offset_pose_g.y += self.current_pose_g.pose.pose.position.y
             self.local_offset_pose_g.z += self.current_pose_g.pose.pose.position.z
 
-        self.local_offset_pose_g.x /= 30.0
-        self.local_offset_pose_g.y /= 30.0
-        self.local_offset_pose_g.z /= 30.0
-        self.local_offset_g /= 30.0
+        self.local_offset_pose_g.x /= 60.0
+        self.local_offset_pose_g.y /= 60.0
+        self.local_offset_pose_g.z /= 60.0
+        self.local_offset_g /= 60.0
 
         self.get_logger().info("coordinate offset set")        
         self.get_logger().info("The X-Axis is facing: {}".format(self.local_offset_g))
+        self.frame_initialized = True
 
 
+    def wait4connect(self):
+        self.get_logger().info("Waiting for connection...")
+        while not self.current_state_g.connected:
+            rclpy.spin_once(self, timeout_sec=0.1)
+            time.sleep(0.1)
 
-    #     self.local_offset_g = 0.0
-    #     self.local_offset_pose_g = Point()
-    #     self.local_pos_pub = self.create_publisher(
-    #         PoseStamped, "/local_position", 10)
-        
-    #     self.global_pos_sub = self.create_subscription(
-    #         Odometry, "/mavros/global_position/local", self.gps_callback, qos_profile=qos_profile)
-    # def pose_cb(self, msg):
-    #     self.get_logger().info("called pose_cb")
-    #     self.local_offset_g, self.local_offset_pose_g = self.calculate_local_offset(msg)
-
-    # def calculate_local_offset(self, msg):
-    #     self.get_logger().info("called calculate_local_offset")
-    #     q0, q1, q2, q3 = (
-    #         msg.pose.pose.orientation.w,
-    #         msg.pose.pose.orientation.x,
-    #         msg.pose.pose.orientation.y,
-    #         msg.pose.pose.orientation.z,
-    #     )
-
-    #     psi = atan2((2 * (q0 * q3 + q1 * q2)),
-    #                 (1 - 2 * (pow(q2, 2) + pow(q3, 2))))
-
-    #     local_offset_g = degrees(psi)
-
-    #     local_offset_pose = Point()
-    #     local_offset_pose.x = msg.pose.pose.position.x
-    #     local_offset_pose.y = msg.pose.pose.position.y
-    #     local_offset_pose.z = msg.pose.pose.position.z
-
-    #     return local_offset_g, local_offset_pose
-
-    # def gps_callback(self, msg):
-    #     self.get_logger().info("called gps_callback")
-
-    #     self.pose_cb(msg)
-    #     global_x = msg.pose.pose.position.x
-    #     global_y = msg.pose.pose.position.y
-    #     global_z = msg.pose.pose.position.z
-
-    #     local_x = global_x * cos(radians(self.local_offset_g)) - global_y * sin(radians(self.local_offset_g))
-    #     local_y = global_x * sin(radians(self.local_offset_g)) + global_y * cos(radians(self.local_offset_g))
-    #     local_z = global_z
-
-
-
-    #     local_pose = PoseStamped()
-    #     local_pose.header.stamp = self.get_clock().now().to_msg()
-    #     local_pose.pose.position.x = local_x
-    #     local_pose.pose.position.y = local_y
-    #     local_pose.pose.position.z = local_z
-
-    #     local_pose.pose.orientation.x = 0.0
-    #     local_pose.pose.orientation.y = 0.0
-    #     local_pose.pose.orientation.z = 0.0
-    #     local_pose.pose.orientation.w = 1.0        
-        
-    #     self.local_pos_pub.publish(local_pose)
-
+        else: 
+            if self.current_state_g.connected:
+                self.get_logger().info("Connected to FCU")
+            else:
+                self.get_logger().info("Connection failed")
+                return -1
 
 def main(args=None):
     rclpy.init(args=args)
